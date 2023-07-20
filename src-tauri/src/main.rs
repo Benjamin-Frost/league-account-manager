c569dd6ff166c2d3e40c617558fb62d09fc3a0e0
Benjamin Frost <bfrost.schule@gmail.com>
2023-07-20 10:37:57 +0200

    feat: improve error handling on lcu login

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use reqwest::ClientBuilder;
+use reqwest::{ClientBuilder, StatusCode};
 use sysinfo::{System, SystemExt};
+use serde::{Serialize, Deserialize};
 
 #[tauri::command]
-async fn lcu_login(url: &str, secret: &str, username: &str, password: &str) -> Result<(), String> {
+async fn lcu_login(url: String, secret: String, username: String, password: String) -> Result<(), Error> {
     if is_league_client_running() {
-        return Err("League Client already running".into());
+        return Err(Error::AlreadyLoggedInError);
     }
     
     let client = ClientBuilder::new()
@@ -15,16 +16,31 @@ async fn lcu_login(url: &str, secret: &str, username: &str, password: &str) -> R
         .build()
         .unwrap();
 
-    let res = client
+    let request = client
     .put(url)
     .basic_auth("riot", Some(secret))
     .body(format!("{{\"username\": \"{}\", \"password\": \"{}\", \"persistLogin\": false}}", username, password))
     .send()
     .await;
 
-    match res {
-        Ok(_) => Ok(()),
-        Err(_) =>  Err("Authentication failed".into())
+    let response = request.unwrap();
+    let status = response.status();
+
+    if status != StatusCode::CREATED {
+        return Err(Error::RiotClientNotRunningError);
+    }
+
+    let body = response.text().await.unwrap();
+    let json = serde_json::from_str::<RiotAuthResponse>(&body).unwrap();
+
+    if json.error == "auth_failure" {
+        return Err(Error::WrongCredentialsError);
+    }
+
+    if json.response_type == "authenticated" {
+        return Ok(())
+    } else {
+        return Err(Error::UnknownError)
     }
 }
 
@@ -34,6 +50,36 @@ fn is_league_client_running() -> bool {
         return true;
     }
     false
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RiotAuthResponse {
+    error: String,
+    #[serde(rename = "type")]
+    response_type: String,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("You are already logged in")]
+    AlreadyLoggedInError,
+    #[error("Please start the League Client first")]
+    RiotClientNotRunningError,
+    #[error("The account credentials are wrong")]
+    WrongCredentialsError,
+    #[error("An unknown error occured")]
+    UnknownError,
+}
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer 
+    {
+        match self {
+            _ => serializer.serialize_str(self.to_string().as_ref()) 
+        }
+    }
 }
 
 fn main() {
--- src/utils/lcu.ts
+++ src/utils/lcu.ts
@@ -18,7 +18,15 @@ const parseLockfile = async () => {
 };
 
 export const login = async (username: string, password: string) => {
-  const { port, secret, protocol } = await parseLockfile();
-  const url = `${protocol}://127.0.0.1:${port}/rso-auth/v1/session/credentials`;
-  await invoke('lcu_login', { url, secret, username, password });
+  try {
+    const { port, secret, protocol } = await parseLockfile();
+    const url = `${protocol}://127.0.0.1:${port}/rso-auth/v1/session/credentials`;
+    await invoke('lcu_login', { url, secret, username, password });
+  } catch (err) {
+    if (err instanceof String) {
+      throw new Error(err.toString());
+    } else {
+      throw new Error('An unknown error occured');
+    }
+  }
 };
