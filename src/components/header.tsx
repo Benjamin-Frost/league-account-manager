463aa4cb29e162fca7c727e70db2fddd258aa191
Benjamin Frost <bfrost.schule@gmail.com>
2023-07-20 20:50:52 +0200

    feat: proper app name, repo button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import githubLogo from '../assets/github-mark-white.svg';
+
 interface HeaderProps {
   onAddAccountClick: () => void;
 }
@@ -11,9 +13,16 @@ export function Header({ onAddAccountClick }: HeaderProps) {
         </h2>
       </div>
       <div className="mt-4 flex md:ml-4 md:mt-0">
+        <a
+          href="https://github.com/Benjamin-Frost/league-account-manager"
+          target="_blank"
+          className="inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+        >
+          <img src={githubLogo} alt="Github Logo" className="w-5 h-5" />
+        </a>
         <button
           type="button"
-          className="inline-flex items-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+          className="ml-3 inline-flex items-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
           onClick={onAddAccountClick}
         >
           Add Account
